b1c856314a0a24b994ece26b324d284735200ff6
Niraviman-Singha <niraviman15-11689@diu.edu.bd>
2024-11-14 10:02:51 +0600

    1016 - Distance

---
+++ src/main/kotlin/Beginner/1016.kt
@@ -0,0 +1,12 @@
+package Beginner
+
+import java.util.Scanner
+fun main(args: Array<String>) {
+
+    var input = Scanner(System.`in`)
+
+    var a = input.nextInt()
+    var distance = a*2
+
+    println("${distance} minutos")
+}
